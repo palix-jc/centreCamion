9d3529e4ce6b39424bf7c75f7c517a14713381b4
palix-jc <mister-boyz@hotmail.fr>
2017-06-24 14:39:54 +0000

    main
    creation meteo
    affichage meteo
    semaphore 2 quais
    mutex pesage

--- main.c
+++ main.c
@@ -2,27 +2,86 @@
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
+#include <semaphore.h>
 
-void *F_thread(void *arg){
+sem_t quai;
+
+pthread_cond_t condition_meteo = PTHREAD_COND_INITIALIZER; /* Création de la condition */
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; /* Création du mutex */
+
+void *Meteo(void *arg){ //gere meteo
+
     char *tab[5] = {"Neige", "Vent", "Soleil", "Brouillard", "Pluie"};
-    //char meteo ;
-    srand(time(NULL));
-    for (int i = 0 ; i < 20 ; i++)
+    srand(time(NULL)); //permet de vrai valeur aléatoire
+    while(1)
     {
-        //meteo = tab[rand() % 5];
-        printf("%s\n", tab[rand() % 5]);
-        //printf("%s\n", meteo);
-        sleep(1);
+        printf("Changement de la meteo pour : %s\n", tab[rand()%5]);
+        sleep(4);
     }
+    return 0;
+}
+
+void *Camion(void *arg){ // thread camion
+
+    sleep(1); //evite les erreurs
+
+    printf("le camion %u attend le quai \n", (unsigned int)pthread_self());
+
+    /* utilisation du semaphore quai */
+    sem_wait(&quai);
+        printf("  le camion %u se fait charger \n", (unsigned int)pthread_self());
+        sleep(2);
+    sem_post(&quai);
+    /* fin d'utilisation du semaphore quai */
+
+    printf("  le camion %u est charge \n", (unsigned int)pthread_self());
+    printf("le camion %u attend le pesage \n", (unsigned int)pthread_self());
+
+    /* utilisation du mutex pesage */
+    pthread_mutex_lock (&mutex); /* On verrouille le mutex */
+    pthread_cond_signal (&condition_meteo); /* On délivre le signal : condition remplie */
+    pthread_cond_wait(&condition_meteo, &mutex);
+
+        printf("pesage du camion %u en cours\n", (unsigned int)pthread_self());
+            sleep(4);
+        srand(time(NULL)); //pour l'aléatoire
+
+        printf("    le camion %u pese %d Kg \n", (unsigned int)pthread_self(), (rand()%(5001)+1000));
+        printf("    le camion %u part \n", (unsigned int)pthread_self());
+
+    pthread_cond_broadcast(&condition_meteo);
+    pthread_mutex_unlock (&mutex); /* On déverrouille le mutex */
+    /* fin d'utilisation du mutex pesage */
+
+    pthread_exit(NULL); //fin du thread
+
     return 0;
 }
 
 int main(void)
 {
-    pthread_t t1;
-    pthread_create (&t1, NULL, F_thread, NULL);
 
+    sem_init(&quai, 0, 2);  //initialisation du semaphore quai
 
-    sleep(40);
+    pthread_t camion1;
+    pthread_t camion2;
+    pthread_t camion3;
+    pthread_t camion4;
+    pthread_t meteo;
+
+    pthread_create (&camion1, NULL, Camion, NULL);
+    pthread_create (&camion2, NULL, Camion, NULL);
+    pthread_create (&camion3, NULL, Camion, NULL);
+    pthread_create (&camion4, NULL, Camion, NULL);
+    pthread_create (&meteo, NULL, Meteo, NULL); //meteo a cree en dernier pour les id
+
+    //le programme s'arrete quand les threads camion ont fini
+    #if 1
+    pthread_join (camion1, NULL);
+    pthread_join (camion2, NULL);
+    pthread_join (camion3, NULL);
+    pthread_join (camion4, NULL);
+    #endif
+
     return 0;
 }
